a2d8f79a1d8001ee05497f137eb6a22d716cdf18
thomasrs <rb311515@dal.ca>
2017-06-01 12:53:15 -0300

    MainActivity view created with three imageButtons to lead to view fragments

--- mobile/src/main/java/com/example/dylanpomeroy/myapplication/MainActivity.java
+++ mobile/src/main/java/com/example/dylanpomeroy/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.dylanpomeroy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends FragmentViews {
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
