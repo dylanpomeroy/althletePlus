ba0ec85a6327a4701fbb725bd730f149434d8668
thomasrs <rb311515@dal.ca>
2017-06-02 12:25:58 -0300

    MainActivity view created with three imageButtons to lead to view fragments merged into the Pedometer_Activity

--- mobile/src/main/java/com/example/dylanpomeroy/myapplication/PedometerActivity.java
+++ mobile/src/main/java/com/example/dylanpomeroy/myapplication/PedometerActivity.java
@@ -1,9 +1,10 @@
 package com.example.dylanpomeroy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class PedometerActivity extends AppCompatActivity {
+public class PedometerActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
