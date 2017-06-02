92eac89b091920a1d095a175d3de0b885c969b64
Andrew Sealy <andrewjsealy@live.ca>
2017-06-02 14:11:11 -0300

    pedometer espresso tests

--- mobile/src/androidTest/java/com/example/dylanpomeroy/myapplication/ExampleInstrumentedTest.java
+++ mobile/src/androidTest/java/com/example/dylanpomeroy/myapplication/ExampleInstrumentedTest.java
@@ -7,12 +7,16 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -37,7 +41,30 @@ public class ExampleInstrumentedTest {
 
     }
 
+    @Test
+    public void checHigh() throws Exception{
+        onView(withId(R.id.high_input)).perform(typeText(String.valueOf("15")));
+
+        onView(withId(R.id.high_input)).check(matches(withText("15")));
+    }
+
+    @Test
+    public void checkLow() throws Exception{
+        onView(withId(R.id.low_input)).perform(typeText(String.valueOf("15")));
+
+        onView(withId(R.id.low_input)).check(matches(withText("15")));
+
+    }
+
+    @Test
+    public void reset()throws Exception{
+        onView(withId(R.id.reset)).perform(click());
+        onView(withId(R.id.text_steps)).check(matches(withText("0 Steps")));
+    }
+
+
 
 
 
 }
+
