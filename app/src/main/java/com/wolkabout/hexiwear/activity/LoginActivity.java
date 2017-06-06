3d4fc7c6a8c2ec2676086b586e6e02b2a90b4503
Dylan Pomeroy <dylan.pomeroy@dal.ca>
2017-06-06 12:22:57 -0300

    added simplified auto-login and previous hexi project

--- app/src/main/java/com/wolkabout/hexiwear/activity/LoginActivity.java
+++ app/src/main/java/com/wolkabout/hexiwear/activity/LoginActivity.java
@@ -22,30 +22,15 @@ package com.wolkabout.hexiwear.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.wolkabout.hexiwear.R;
-import com.wolkabout.hexiwear.util.Dialog;
-import com.wolkabout.hexiwear.view.Input;
 import com.wolkabout.wolkrestandroid.Credentials_;
 import com.wolkabout.wolkrestandroid.dto.AuthenticationResponseDto;
-import com.wolkabout.wolkrestandroid.dto.EmailVerificationRequest;
 import com.wolkabout.wolkrestandroid.dto.SignInDto;
 import com.wolkabout.wolkrestandroid.service.AuthenticationService;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
-import org.androidannotations.annotations.Bean;
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.EditorAction;
-import org.androidannotations.annotations.SystemService;
-import org.androidannotations.annotations.UiThread;
-import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 import org.androidannotations.rest.spring.annotations.RestService;
 import org.springframework.http.HttpStatus;
@@ -56,83 +41,13 @@ public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = LoginActivity.class.getSimpleName();
 
-    @ViewById
-    Input emailField;
-
-    @ViewById
-    Input passwordField;
-
-    @ViewById
-    View signInElements;
-
-    @ViewById
-    TextView signingInLabel;
-
-    @ViewById
-    View signingInElements;
-
-    @ViewById
-    Button signInButton;
-
     @Pref
     Credentials_ credentials;
-
-    @SystemService
-    InputMethodManager inputMethodManager;
-
-    @Bean
-    Dialog dialog;
 
     @RestService
     AuthenticationService authenticationService;
 
     public void onCreate(){
-        signIn();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        String dataString = getIntent().getDataString();
-        Log.i(TAG, "Data string: " + dataString);
-        if (dataString != null && dataString.contains("activationCode=")) {
-            Log.i(TAG, "Activate");
-            verifyEmail(dataString.substring(dataString.indexOf('=') + 1));
-        }
-    }
-
-    @AfterViews
-    void startMainActivity() {
-        if (credentials.username().exists() && !credentials.username().get().equals("Demo")) {
-            MainActivity_.intent(this).start();
-            finish();
-        }
-    }
-
-    @Click(R.id.signUp)
-    void openSignUpScreen() {
-        SignUpActivity_.intent(this).start();
-    }
-
-    @Click(R.id.signInButton)
-    @EditorAction(R.id.passwordField)
-    void attemptSignIn() {
-//        if ("demo".equals(emailField.getValue()) && "demo".equals(passwordField.getValue())) {
-//            credentials.username().put("Demo");
-//            MainActivity_.intent(LoginActivity.this).start();
-//            finish();
-//            return;
-//        }
-//
-//        if (!validateCredentials()) {
-//            return;
-//        }
-//
-//        inputMethodManager.hideSoftInputFromWindow(passwordField.getWindowToken(), 0);
-//
-//        signInElements.setVisibility(View.GONE);
-//        signingInElements.setVisibility(View.VISIBLE);
-
         signIn();
     }
 
@@ -153,58 +68,11 @@ public class LoginActivity extends AppCompatActivity {
         } catch (HttpStatusCodeException e) {
             Log.e(TAG, "signIn: ", e);
             if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                onSignInError(R.string.login_error_invalid_credentials);
                 return;
             }
 
-            onSignInError(R.string.login_error_general);
         } catch (Exception e) {
             Log.e(TAG, "signIn: ", e);
-            onSignInError(R.string.login_error_general);
         }
     }
-
-    @UiThread
-    void onSignInError(int messageRes) {
-        signInElements.setVisibility(View.VISIBLE);
-        signingInElements.setVisibility(View.GONE);
-        dialog.showWarning(messageRes);
-    }
-
-    @Click
-    void resetPassword() {
-        ResetPasswordActivity_.intent(this).start();
-    }
-
-    private boolean validateCredentials() {
-        if (emailField.isEmpty()) {
-            emailField.setError(R.string.registration_error_email_required);
-            return false;
-        }
-
-        if (passwordField.isEmpty()) {
-            passwordField.setError(R.string.registration_error_password_required);
-            return false;
-        }
-
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailField.getValue()).matches()) {
-            emailField.setError(R.string.registration_error_invalid_email);
-            return false;
-        }
-
-        return true;
-    }
-
-    @Background
-    void verifyEmail(String code) {
-        try {
-            Log.i(TAG, "Verifying email - code: " + code);
-            final EmailVerificationRequest request = new EmailVerificationRequest(code);
-            authenticationService.verifyEmail(request);
-            Toast.makeText(LoginActivity.this, "Email verified.", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Toast.makeText(LoginActivity.this, "Failed to verify email.", Toast.LENGTH_LONG).show();
-        }
-    }
-
 }
